9b9f74e3939487dfb0ec977904b56536f3f88e3f
Yewon Kwak <goodywk07@gmail.com>
2021-01-16 16:33:37 -0500

    frontend from tutorial

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+    import {
+      Button,
+      Modal,
+      ModalHeader,
+      ModalBody,
+      ModalFooter,
+      Form,
+      FormGroup,
+      Input,
+      Label
+    } from "reactstrap";
+
+    export default class CustomModal extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          activeItem: this.props.activeItem
+        };
+      }
+      handleChange = e => {
+        let { name, value } = e.target;
+        if (e.target.type === "checkbox") {
+          value = e.target.checked;
+        }
+        const activeItem = { ...this.state.activeItem, [name]: value };
+        this.setState({ activeItem });
+      };
+      render() {
+        const { toggle, onSave } = this.props;
+        return (
+          <Modal isOpen={true} toggle={toggle}>
+            <ModalHeader toggle={toggle}> Todo Item </ModalHeader>
+            <ModalBody>
+              <Form>
+                <FormGroup>
+                  <Label for="title">Name of food item</Label>
+                  <Input
+                    type="text"
+                    name="title"
+                    value={this.state.activeItem.title}
+                    onChange={this.handleChange}
+                    placeholder="Enter Food Name"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="description">Description</Label>
+                  <Input
+                    type="text"
+                    name="description"
+                    value={this.state.activeItem.description}
+                    onChange={this.handleChange}
+                    placeholder="Enter Food description"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="expiration">Expiration Date</Label>
+                  <Input
+                    type="date"
+                    name="expiration"
+                    value={this.state.activeItem.expiration}
+                    onChange={this.handleChange}
+                    placeholder="Enter expiration date"
+                  />
+                </FormGroup>
+                <FormGroup check>
+                  <Label for="completed">
+                    <Input
+                      type="checkbox"
+                      name="completed"
+                      checked={this.state.activeItem.completed}
+                      onChange={this.handleChange}
+                    />
+                    Used
+                  </Label>
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+                Save
+              </Button>
+            </ModalFooter>
+          </Modal>
+        );
+      }
+    }
